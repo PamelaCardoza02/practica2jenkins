aaa16c014845a8ea254a8c456eeb16841c902a79
Pamela <Cardozapamela0@gmail.com>
2022-03-23 21:47:38 -0700

    Cambio de mensaje

--- prueba.java
+++ prueba.java
@@ -2,7 +2,7 @@ class prueba{
 	public static void main (String [] args){
 	
 	System.out.println("Esta es la practica 2 de jenkins, saludos");
-	
+	System.out.println("Se ha confirmado la conexion");
 	}
 
 
